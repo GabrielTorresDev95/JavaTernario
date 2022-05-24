e058c3fef9f94be7916521704b8242f9ff118c83
Gabrielzint <66974496+Gabrielzint@users.noreply.github.com>
2022-05-24 17:15:58 -0300

    Add files via upload

---
+++ Ternario.java
@@ -0,0 +1,21 @@
+package FundamentosIniciais;
+
+public class Ternario {
+	public static void main(String[] args) {
+		
+		double media = 5.0;
+		String resultadoParcial = media >= 6.0 ? "aprovado" : "recuperacao";
+		
+;		
+		System.out.println("O aluno esta: " + resultadoParcial);
+		
+		double mediaFinal = 7.0;
+		String resultadoFinal = mediaFinal >= 7.0 ? "aprovado" : "reprovado";
+		System.out.println("O aluno esta: " + resultadoFinal);
+		
+		
+		
+		
+		
+	}
+}
